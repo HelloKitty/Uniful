2b9eff5f5e9e82c4c1a4c512d78f3ce433ba3513
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-15 22:28:01 -0500

    Added LevelLoading and OnSceneStart Components

---
+++ src/Uniful/Level/LoadNewLevel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Loading
+{
+	/// <summary>
+	/// Exposes Unity3D API <see cref="Start"/> level loading functionality
+	/// which can be used to abstract level loading logic or be the target of <see cref="UnityEngine.Events.UnityEvent"/> callbacks.
+	/// </summary>
+	public class LoadNewLevel : MonoBehaviour
+	{
+		/// <summary>
+		/// Loads the level/scene with the given id.
+		/// </summary>
+		/// <param name="id"></param>
+		public void LoadLevel(int id)
+		{
+			//TODO: Change to SceneManager when the likelyhood of someone using a Unity version without it becomes low.
+			Application.LoadLevel(id);
+		}
+
+		/// <summary>
+		/// Loads the level/scene with the specified name.
+		/// </summary>
+		/// <param name="levelName">Level name.</param>
+		public void LoadLevel(string levelName)
+		{
+			if (String.IsNullOrEmpty(levelName))
+				throw new ArgumentException("Cannot load a null or empty named scene/level.", nameof(levelName));
+
+			//TODO: Change to SceneManager when the likelyhood of someone using a Unity version without it becomes low.
+			Application.LoadLevel(levelName);
+		}
+	}
+}
---
+++ src/Uniful/Level/OnSceneStart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Uniful
+{
+	/// <summary>
+	/// Exposes Unity3D API <see cref="Start"/> magic method to the inspector and offers
+	/// callbacks using <see cref="UnityEngine.Events.UnityEvent"/>.
+	/// </summary>
+	public class OnSceneStart : MonoBehaviour
+	{
+		/// <summary>
+		/// Exposes a callback list for subscribers to listen for when the the current scene begins.
+		/// </summary>
+#pragma warning disable 0649
+		[SerializeField]
+		private UnityEvent OnSceneStartEvent;
+#pragma warning restore 0649
+
+		void Start()
+		{
+			if (OnSceneStartEvent != null)
+				OnSceneStartEvent.Invoke();
+		}
+	}
+}
