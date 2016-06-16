b33ab2bd2686688ec15e7cad83b8dbffb487c6b4
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-15 22:41:32 -0500

    Added GameObjectRotation component

---
+++ src/Uniful/GameObject/Transformation/RotateGameObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Animation
+{
+	/// <summary>
+	/// Exposes Unity3D API for GameObject transform rotation.
+	/// </summary>
+	public class RotateGameObject : MonoBehaviour
+	{
+		/// <summary>
+		/// Speed at which to rotate the object.
+		/// </summary>
+		[SerializeField]
+		private float speed;
+
+		/// <summary>
+		/// Unit vector direction to rotate the object in.
+		/// </summary>
+		[SerializeField]
+		private Vector3 direction;
+
+		/// <summary>
+		/// Indicates if <see cref="FixedUpdate"/> should be used.
+		/// If false <see cref="Update"/> is used.
+		/// These a magic Unity methods so review that documentation.
+		/// </summary>
+#pragma warning disable 0649
+		[SerializeField]
+		protected bool _useFixedUpdate;
+#pragma warning restore 0649
+
+		void Update()
+		{
+			if(!_useFixedUpdate)
+				this.gameObject.transform.Rotate(direction * (speed * Time.deltaTime), Space.World);
+		}
+
+		void FixedUpdate()
+		{
+			if(_useFixedUpdate)
+				this.gameObject.transform.Rotate(direction * (speed * Time.deltaTime), Space.World);
+		}
+	}
+}
